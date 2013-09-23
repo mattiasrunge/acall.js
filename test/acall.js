330382cfd59f398c238466e02d4d62e1af679c11
Mattias Runge <mattias@runge.se>
2013-09-23 19:37:32 +0200

    Added unit tests and linted the code.

---
+++ test/acall.js
@@ -0,0 +1,50 @@
+
+var acall = require("../acall.js");
+var assert = require("assert");
+
+suite("acall.js", function()
+{
+  test("Just a simple command", function(done)
+  {
+    acall("date", done);
+  });
+
+  test("One parameter and check output", function(done)
+  {
+    acall([ "echo", "Hello World!"], function(error, output)
+    {
+      if (error)
+      {
+        done(error);
+        return;
+      }
+
+      assert.equal("Hello World!\n", output);
+      done();
+    });
+  });
+
+  test("One parameter and an option", function(done)
+  {
+    acall([ "ls", "-l" ], { cwd: "/" }, function(error, output)
+    {
+      if (error)
+      {
+        done(error);
+        return;
+      }
+
+      assert.ok(output.length > 0);
+      done();
+    });
+  });
+
+  test("Check that errors are propagated", function(done)
+  {
+    acall("a_comman_that_should_not_exist", function(error)
+    {
+      assert.ok(error.indexOf("Exited with result") === 0);
+      done();
+    });
+  });
+});
